f08d997fefe90e0a92156d6744c6f06817cadf8f
Davi Lima <davim5@hotmail.com>
2021-05-26 15:58:29 -0300

    First commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+    colors:{
+        highlight: "#FFBA08",
+        highlight50: "rgba(255, 186, 8, 0.5)",
+        dark:{
+            black:"#000000",
+            text: "#47585B",
+            info: "#999999",
+            info50: "rgba(153, 153, 153, 0.5)",
+        },
+        light: {
+            white:"#FFFFFF",
+            text:"#F5F8FA",
+            info:"#DADADA"
+        }
+    },
+    styles: {
+        global:{
+            body:{
+                bg:"#FFFFFF",
+                color:"#47585B",
+            },
+        },
+    },
+})
